55615eba7fe4fc5082ba3ece4f8cbe6033dcc4a9
sammilechman <sammilechman@gmail.com>
2015-12-27 14:40:34 -0800

    authorize user before creating occurrence

--- app/controllers/occurrences_controller.rb
+++ app/controllers/occurrences_controller.rb
@@ -1,11 +1,12 @@
 class OccurrencesController < ApplicationController
   def create
     habit = Habit.find(params[:habit_id])
+    authorize!(:manage, habit)
     if habit.create_occurrence!
       flash[:notice] = 'Success'
     else
       flash[:alert] = 'Error'
     end
-    redirect_to habit_url(habit)
+    redirect_to habits_url
   end
 end
