c291ff70345eb7caab62bea7f9f3ca7404a0aa36
sammilechman <sammilechman@gmail.com>
2016-03-20 10:43:27 -0700

    add a first habits controller test

--- spec/controllers/habits_controller_spec.rb
+++ spec/controllers/habits_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe HabitsController, type: :controller do
+  include Devise::TestHelpers
 
+  before :each do
+    @user = FactoryGirl.create(:user)
+    sign_in @user
+  end
+
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
 end
